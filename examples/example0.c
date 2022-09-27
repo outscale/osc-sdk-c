aad5306955c86b28579c0c0b9c31065f2a976d0e
Matthias Gatto <matthias.gatto@outscale.com>
2022-09-27 11:43:31 +0200

    reindent example
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- examples/example0.c
+++ examples/example0.c
@@ -2,22 +2,23 @@
 
 int main(void)
 {
-        struct osc_env e;
-        struct osc_str r;
+	struct osc_env e;
+	struct osc_str r;
 
-        if (osc_init_sdk(&e, NULL, 0) < 0)
+	if (osc_init_sdk(&e, NULL, 0) < 0)
 		return 1;
-        osc_init_str(&r);
+	osc_init_str(&r);
 
-        osc_read_load_balancers(&e, &r, NULL);
-        printf("[%s", r.buf);
-        osc_deinit_str(&r);
+	osc_read_load_balancers(&e, &r, NULL);
+	printf("[%s", r.buf);
+	osc_deinit_str(&r);
 
-        osc_read_images(&e, &r, &(struct osc_read_images_arg)
-			{.filters="{\"AccountAliases\": [\"Outscale\"]}"}
+	osc_read_images(&e, &r, &(struct osc_read_images_arg)
+			{.filters_str="{\"AccountAliases\": [\"Outscale\"]}"}
 		);
-        printf(",\n%s]\n", r.buf);
+	printf(",\n%s]\n", r.buf);
 
-        osc_deinit_str(&r);
-        osc_deinit_sdk(&e);
+	osc_deinit_str(&r);
+	osc_deinit_sdk(&e);
+	return 0;
 }
