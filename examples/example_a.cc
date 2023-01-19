6969730c7abc13850bc30d191cb07e09352a9a94
Matthias Gatto <matthias.gatto@outscale.com>
2023-01-19 15:12:54 +0100

    C++ example_a
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ examples/example_a.cc
@@ -0,0 +1,34 @@
+/* ANSI C compatible example */
+
+extern "C" {
+#include "osc_sdk.h"
+}
+
+int main(void)
+{
+	struct osc_env e;
+	struct osc_str r;
+	struct osc_read_images_arg arg = {0};
+	char str[] = "{\"AccountAliases\": [\"Outscale\"]}";
+	int ret = 1;
+
+	arg.filters_str = str;
+	if (osc_init_sdk(&e, NULL, 0) < 0)
+		return 1;
+	osc_init_str(&r);
+
+	if (osc_read_load_balancers(&e, &r, NULL))
+		goto out;
+	printf("== osc_read_load_balancers ==\n%s\n", r.buf);
+	osc_deinit_str(&r);
+
+	if (osc_read_images(&e, &r, &arg) < 0)
+		goto out;
+	printf("== osc_read_images ==\n%s\n", r.buf);
+
+	ret = 1;
+out:
+	osc_deinit_str(&r);
+	osc_deinit_sdk(&e);
+	return ret;
+}
