446019033a5489c369f51f99ecc1d3450abfbe98
Matthias Gatto <matthias.gatto@outscale.com>
2023-05-04 10:40:14 +0200

    auto trigger oapi-cli on release
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ .github/scripts/build-osc-sdk.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+root=$(cd "$(dirname $0)/../.." && pwd)
+API_VERSION=$(cat $root/osc_sdk.h | grep OSC_API_VERSION | cut -d '"' -f 2)
+
+WHICH_SDK=$1
+BRANCH="master"
+
+if [ $# -gt 1 ]; then
+    BRANCH=$2
+fi
+
+if [ -z "$API_VERSION" ]; then
+    echo "API_VERSION is missing, abort"
+    echo "pass API_VERSION through environment variable"
+    exit 1
+fi
+
+if [ -z "$GH_TOKEN" ]; then
+    echo "GH_TOKEN is missing, abort."
+    echo "pass GH_TOKEN through environment variable"
+    exit 1
+fi
+
+ghurl="https://api.github.com"
+data="{\"ref\": \"${BRANCH}\", \"inputs\": {\"api_version\": \"$API_VERSION\"}"
+curl \
+    -X POST \
+    -H "Authorization: token $GH_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -d "${data}" \
+    "${ghurl}/repos/outscale/${WHICH_SDK}/actions/workflows/build.yml/dispatches"
